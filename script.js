e15e379a57173d2d14fcbfd7d0f55f3b9e3e1f83
Kelton Hale <keltonhale33@gmail.com>
2021-11-10 10:44:37 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const app = () => {
+  const el = document.getElementById("tasks");
+  const tasks = [];
+
+  const FetchAll = function () {};
+
+  const Add = function () {};
+
+  const Edit = function (item) {};
+};
